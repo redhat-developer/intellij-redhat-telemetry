efcbcd1bc431f5cb7fffcb6cc3ac4d63223772c8
Andre Dietisheim <adietish@redhat.com>
2021-03-17 17:41:06 +0100

    feat: allow to deduce the country from the timezone (#18)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

---
+++ src/test/java/com/redhat/devtools/intellij/telemetry/core/service/CountryTest.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.telemetry.core.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.TimeZone;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CountryTest {
+
+    @Test
+    void get_should_return_country_for_timezoneId() {
+        // given
+        // when
+        String country = Country.getInstance().get(TimeZone.getTimeZone("Europe/Zurich"));
+        // then
+        assertThat(country).isEqualTo("Switzerland");
+    }
+
+    @Test
+    void get_should_return_null_for_null_timezoneId() {
+        // given
+        // when
+        String country = Country.getInstance().get(null);
+        // then
+        assertThat(country).isEqualTo(null);
+    }
+
+    @Test
+    void get_should_return_timezoneId_for_unknown_timezoneId() {
+        // given
+        // when
+        TimeZone timeZone = TimeZone.getDefault();
+        timeZone.setID("Aldreean/Organa Major");
+        String country = Country.getInstance().get(timeZone);
+        // then
+        assertThat(country).isEqualTo("Aldreean/Organa Major");
+    }
+
+    @Test
+    void get_should_return_country_for_timezoneId_with_alternative() {
+        // given
+        // when "America/Argentina/ComodRivadavia" -> "a: America/Argentina/Catamarca" -> "c: AR" -> "Argentina"
+        String country = Country.getInstance().get(TimeZone.getTimeZone("America/Argentina/ComodRivadavia"));
+        // then
+        assertThat(country).isEqualTo("Argentina");
+    }
+
+    @Test
+    void get_should_return_timezoneId_for_timezoneId_without_country_nor_alternative() {
+        // given
+        // when
+        String country = Country.getInstance().get(TimeZone.getTimeZone("CET"));
+        // then
+        assertThat(country).isEqualTo("CET");
+    }
+
+}
