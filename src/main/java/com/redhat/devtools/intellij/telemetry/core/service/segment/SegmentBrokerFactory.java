e729c19540b010f55137e0f45a959ab44a5a687c
Stephane Bouchet <sbouchet@redhat.com>
2024-06-17 15:55:30 +0200

    refactor builder to use plugindescriptor
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/telemetry/core/service/segment/SegmentBrokerFactory.java
+++ src/main/java/com/redhat/devtools/intellij/telemetry/core/service/segment/SegmentBrokerFactory.java
@@ -1,5 +1,6 @@
 package com.redhat.devtools.intellij.telemetry.core.service.segment;
 
+import com.intellij.openapi.extensions.PluginDescriptor;
 import com.redhat.devtools.intellij.telemetry.core.IMessageBroker;
 import com.redhat.devtools.intellij.telemetry.core.service.Environment;
 import com.redhat.devtools.intellij.telemetry.core.service.IDE;
@@ -10,9 +11,9 @@ import static com.redhat.devtools.intellij.telemetry.core.IMessageBroker.*;
 public class SegmentBrokerFactory implements IMessageBrokerFactory {
 
     @Override
-    public IMessageBroker create(boolean isDebug, ClassLoader classLoader) {
-        Environment environment = createEnvironment(classLoader);
-        SegmentConfiguration configuration = new SegmentConfiguration(classLoader);
+    public IMessageBroker create(boolean isDebug, PluginDescriptor descriptor) {
+        Environment environment = createEnvironment(descriptor);
+        SegmentConfiguration configuration = new SegmentConfiguration(descriptor.getPluginClassLoader());
         return new SegmentBroker(
                 isDebug,
                 UserId.INSTANCE.get(),
@@ -20,13 +21,13 @@ public class SegmentBrokerFactory implements IMessageBrokerFactory {
                 configuration);
     }
 
-    private static Environment createEnvironment(ClassLoader classLoader) {
+    private static Environment createEnvironment(PluginDescriptor descriptor) {
         IDE ide = new IDE.Factory()
                 .create()
                 .setJavaVersion();
         return new Environment.Builder()
                 .ide(ide)
-                .plugin(classLoader)
+                .plugin(descriptor)
                 .build();
     }
 }
